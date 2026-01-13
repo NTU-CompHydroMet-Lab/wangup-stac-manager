774005ad1bc078ad5d49b8a7c862c9c548a45f59
Isongzhe <Isongzhe@users.noreply.github.com>
2026-01-13 22:28:44 +0000

    Deploy catalog from v0.2.0 f8a2195a43944529b6fe827dd9b2e26c4e190514

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    catalogUrl: "./catalog.json",
+    catalogTitle: "NTU CompHydroMet Lab Data Catalog",
+    allowExternalAccess: true,
+    pathPrefix: "/wangup-stac-manager/",
+    historyMode: "hash"
+};
